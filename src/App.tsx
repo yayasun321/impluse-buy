fce08689a30e112178d17fd9721054fee391d096
Yaya <137446062+yayasun321@users.noreply.github.com>
2026-04-16 00:14:38 -0700

    added a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Box from "./Box";
-//import Title from "./Title";
+import Button from "./Button";
 function App() {
   return (
     <div>
       <h1 className="Main-Title">Have an impulse?</h1>
+      <Button onClick={() => console.log("Clicked")}>My Button</Button>
       <Box />
       <h1 className="Purchased-Title">Purchased:</h1>
     </div>
